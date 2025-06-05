663e665d3c8d9ddbe62325cac58b5a815e836274
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-06-05 13:28:02 +0530

    meta-tittle

---
+++ src/app/RootLayoutClient.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { Poppins } from "next/font/google";
+import Script from "next/script";
+import Navbar from "./Common/Navbar";
+import Footer from "./Common/Footer";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export default function RootLayoutClient({ children }) {
+  return (
+    <html lang="en" className={poppins.variable}>
+      <body className="antialiased">
+        {/* Google Tag Manager (script) */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MFSSPQ8C');`,
+          }}
+        />
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MFSSPQ8C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm-noscript"
+          />
+        </noscript>
+
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
