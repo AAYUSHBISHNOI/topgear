a5a6bd112d1f6cafe5bcf93f811b14b9133476e4
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-06-05 15:39:11 +0530

    text-color-change

--- src/app/Components/Ourservicehome.jsx
+++ src/app/Components/Ourservicehome.jsx
@@ -24,7 +24,7 @@ const Ourservicehome = () => {
 
       {/* Section Heading */}
       <div className="text-center flex flex-col justify-center items-center">
-        <p className="text-[#00FDEC] font-Poppins font-semibold leading-[130%] text-2xl md:text-3xl">
+        <p className="text-[#006156] font-Poppins font-semibold leading-[130%] text-2xl md:text-3xl">
           Our Services
         </p>
         <h2 className="text-base sm:text-[26px] md:text-[32px] lg:text-[40px] font-Poppins font-medium leading-[121%] text-[#353535] max-w-[690px] md:max-w-[897px] mt-2">
