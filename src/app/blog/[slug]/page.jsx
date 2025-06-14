6fcb4a83ecb8acaf22d5dd0c9b9c416d53e90038
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-06-14 12:29:15 +0530

    bloglayout-text-change

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,8 @@
 import BlogLayout from "../../Components/BlogLayout";
 
-import DigitalMarketingStrategies from "../../../content/blog/digital-marketing-strategies.jsx";
-import FloatVanCampaign from "../../../content/blog/float-van-campaign.jsx";
-import WhatIsBTLMarketing from "../../../content/blog/what-is-btl-marketing.jsx";
+import FloatVanCampaign from "@/content/blog/float-van-campaign.jsx";
+import WhatIsBTLMarketing from "@/content/blog/what-is-btl-marketing.jsx";
+import DigitalMarketingStrategies from "@/content/blog/digital-marketing-strategies";
 
 const blogMap = {
   "digital-marketing-strategies": DigitalMarketingStrategies,
